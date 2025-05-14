3aa79c64814c380fddfa2c3b82833c07392f64e2
arshitrupanii <arshitrupani6@gmail.com>
2025-05-14 13:30:16 +0530

    app setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/Modetoggle";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>click</Button>
+      <ModeToggle/>
+    </div>
+  );
+}
